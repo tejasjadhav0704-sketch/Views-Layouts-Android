72eaf84a3745d8997285a01387d4a44d73c55475
charliee <Tejas@Tejass-MacBook-Air.local>
2026-05-30 17:12:38 +0530

    Added DailogBox

--- app/src/main/java/com/example/views_practice/MainActivity.kt
+++ app/src/main/java/com/example/views_practice/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.views_practice
 import android.os.Bundle
 import android.widget.Toast
 import android.content.Intent
+import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.views_practice.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,60 +20,146 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.INPUT.setOnClickListener {
-            Toast.makeText(this, "${binding.INPUT.text.toString()}", Toast.LENGTH_SHORT).show()
+        val builder1 = AlertDialog.Builder(this)
+
+        binding.checkBox1.setOnClickListener {
+            builder1.setIcon(R.drawable.outline_circle_24)
+            builder1.setTitle("AlertDailog Practice")
+            builder1.setMessage("U Selected Java")
+            builder1.setPositiveButton("Yes") { _, _ ->
+                Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+            }
+            builder1.setNegativeButton("No") { _, _ ->
+                Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+            }
+            builder1.setNeutralButton("Neutral") { _, _ ->
+                Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+            }
+            builder1.show()
         }
 
-        binding.checkBox1.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                Toast.makeText(this, "Java", Toast.LENGTH_SHORT).show()
+        binding.checkBox2.setOnClickListener {
+            builder1.setIcon(R.drawable.outline_circle_24)
+            builder1.setTitle("AlertDailog Practice")
+            builder1.setMessage("U Selected Kotlin")
+            builder1.setPositiveButton("Yes") { _, _ ->
+                Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
             }
+            builder1.setNegativeButton("No") { _, _ ->
+                Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+            }
+            builder1.setNeutralButton("Neutral") { _, _ ->
+                Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+            }
+            builder1.show()
+        }
 
-            binding.checkBox2.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    Toast.makeText(this, "Kotlin", Toast.LENGTH_SHORT).show()
+        binding.checkBox3.setOnClickListener {
+            builder1.setIcon(R.drawable.outline_circle_24)
+            builder1.setTitle("AlertDailog Practice")
+            builder1.setMessage("U Selected Firebase")
+            builder1.setPositiveButton("Yes") { _, _ ->
+                Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+            }
+            builder1.setNegativeButton("No") { _, _ ->
+                Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+            }
+            builder1.setNeutralButton("Neutral") { _, _ ->
+                Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+            }
+            builder1.show()
+        }
+
+        binding.checkBox4.setOnClickListener {
+            builder1.setIcon(R.drawable.outline_circle_24)
+            builder1.setTitle("AlertDailog Practice")
+            builder1.setMessage("U Selected Java")
+            builder1.setPositiveButton("Yes") { _, _ ->
+                Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+            }
+            builder1.setNegativeButton("No") { _, _ ->
+                Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+            }
+            builder1.setNeutralButton("Neutral") { _, _ ->
+                Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+            }
+            builder1.show()
+        }
+
+        binding.radioGroup.setOnCheckedChangeListener { _, isChecked ->
+            when (isChecked) {
+                binding.radioButton1.id -> {
+                    builder1.setIcon(R.drawable.outline_circle_24)
+                    builder1.setTitle("AlertDailog Practice")
+                    builder1.setMessage("U Selected Android")
+                    builder1.setPositiveButton("Yes") { _, _ ->
+                        Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNegativeButton("No") { _, _ ->
+                        Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNeutralButton("Neutral") { _, _ ->
+                        Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.show()
+                }
+
+                binding.radioButton2.id -> {
+                    builder1.setIcon(R.drawable.outline_circle_24)
+                    builder1.setTitle("AlertDailog Practice")
+                    builder1.setMessage("U Selected Web")
+                    builder1.setPositiveButton("Yes") { _, _ ->
+                        Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNegativeButton("No") { _, _ ->
+                        Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNeutralButton("Neutral") { _, _ ->
+                        Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.show()
+                }
+
+                binding.radioButton3.id -> {
+                    builder1.setIcon(R.drawable.outline_circle_24)
+                    builder1.setTitle("AlertDailog Practice")
+                    builder1.setMessage("U Selected Full-Stack")
+                    builder1.setPositiveButton("Yes") { _, _ ->
+                        Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNegativeButton("No") { _, _ ->
+                        Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNeutralButton("Neutral") { _, _ ->
+                        Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.show()
+                }
+
+                binding.radioButton4.id -> {
+                    builder1.setIcon(R.drawable.outline_circle_24)
+                    builder1.setTitle("AlertDailog Practice")
+                    builder1.setMessage("U Selected DataBase")
+                    builder1.setPositiveButton("Yes") { _, _ ->
+                        Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNegativeButton("No") { _, _ ->
+                        Toast.makeText(this, "No", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.setNeutralButton("Neutral") { _, _ ->
+                        Toast.makeText(this, "Neutral", Toast.LENGTH_SHORT).show()
+                    }
+                    builder1.show()
                 }
             }
+        }
 
-            binding.checkBox3.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    Toast.makeText(this, "FireBase", Toast.LENGTH_SHORT).show()
-                }
-            }
+        binding.fab.setOnClickListener {
+            startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        }
 
-            binding.checkBox4.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    Toast.makeText(this, "MySql", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            binding.radioGroup.setOnCheckedChangeListener { _, ischecked ->
-                when (ischecked) {
-                    binding.radioButton1.id -> {
-                        Toast.makeText(this, "Android selected", Toast.LENGTH_SHORT).show()
-                    }
-
-                    binding.radioButton2.id -> {
-                        Toast.makeText(this, "Web selected", Toast.LENGTH_SHORT).show()
-                    }
-
-                    binding.radioButton3.id -> {
-                        Toast.makeText(this, "Full-Stack selected", Toast.LENGTH_SHORT).show()
-                    }
-
-                    binding.radioButton4.id -> {
-                        Toast.makeText(this, "DataBase", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-
-            binding.fab.setOnClickListener {
-                Toast.makeText(this, "U Clicked FloatingActionButton", Toast.LENGTH_SHORT).show()
-            }
-
-            binding.button2.setOnClickListener {
-                startActivity(Intent(this, MainActivity2::class.java))
-            }
+        binding.button2.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
         }
     }
 }
